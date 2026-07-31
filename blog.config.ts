64c99055308c2775dc5c609278905d1a2acd1b59
明久 <smshnsj@163.com>
2026-07-31 15:20:22 +0800

    更新 Twikoo 地址为 Vercel 新部署

--- blog.config.ts
+++ blog.config.ts
@@ -67,8 +67,8 @@ const blogConfig = {
 
 	/** Twikoo 评论服务（后续部署时替换） */
 	twikoo: {
-		envId: 'https://twikoo-plum-delta.vercel.app/',
-		preload: 'https://twikoo-plum-delta.vercel.app/',
+		envId: 'https://twikoo-api-seven-iota.vercel.app/',
+		preload: 'https://twikoo-api-seven-iota.vercel.app/',
 	},
 }
 
